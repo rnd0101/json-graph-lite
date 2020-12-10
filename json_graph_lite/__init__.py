c4bc103f1d2967075f2525f28663b3f01d592979
Roman Suzi <roman.suzi@talentadore.com>
2020-12-10 20:49:53 +0200

    Renamed JGF graph to JGFGraph

--- json_graph_lite/__init__.py
+++ json_graph_lite/__init__.py
@@ -2,7 +2,5 @@ from .node import Node
 from .edge import Edge
 from .graph import Graph
 from .graphs import GraphList
-from .jgf import Node
-from .jgf import Edge
-from .jgf import Graph
+from .jgf import Graph as JGFGraph
 from .jgf import Graphs
